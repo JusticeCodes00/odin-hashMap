0eeffa9f038e9fc9b2ebf5ef4f53866dd2490352
TheDeveloper <171641141+JusticeCodes00@users.noreply.github.com>
2026-01-29 18:52:50 +0100

    Add spacing

--- classes/HashMap.js
+++ classes/HashMap.js
@@ -1,47 +1,64 @@
 import LinkedList from "./LinkedList.js";
 
 export default class HashMap {
-    #bucket;
-    #capacity;
-    #loadFactor;
-    #entries = 0;
-    constructor() {
-        this.#capacity = 16;
-        this.#bucket = new Array(this.#capacity)
-            .fill(null)
-            .map(() => new LinkedList());
-        this.#loadFactor = 0.75;
+  #bucket;
+  #capacity;
+  #loadFactor;
+  #entries = 0;
+  constructor() {
+    this.#capacity = 16;
+    this.#bucket = new Array(this.#capacity)
+      .fill(null)
+      .map(() => new LinkedList());
+    this.#loadFactor = 0.75;
+  }
+
+  #hash(key) {
+    if (typeof key !== "string") {
+      throw new Error("key must be of type String");
     }
 
-    #hash(key) {
-        if (typeof key !== "string") {
-            throw new Error("key must be of type String");
-        }
+    let hashCode = 0;
+    const primeNum = 31;
 
-        let hashCode = 0;
-        const primeNum = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNum * hashCode + key.charCodeAt(i)) % this.#capacity;
+    }
+    return hashCode;
+  }
 
-        for (let i = 0; i < key.length; i++) {
-            hashCode =
-                (primeNum * hashCode + key.charCodeAt(i)) % this.#capacity;
-        }
-        return hashCode;
+  #growBucket() {
+    this.#capacity = this.#capacity * 2;
+
+    const prevBucket = this.#bucket;
+
+    this.#bucket = new Array(this.#capacity)
+      .fill(null)
+      .map(() => new LinkedList());
+  }
+
+  set(key, value) {
+    if (this.#entries >= this.#loadFactor * this.#capacity) {
+      console.log("Time to resize bucket");
+      this.#growBucket();
+      return;
+    }
+    const bucketIndex = this.#hash(key);
+    const list = this.#bucket[bucketIndex];
+
+    const match = (container) => key in container;
+
+    if (list.contains(match)) {
+      list.at(list.findIndex(match))[key] = value;
     }
 
-    set(key, value) {
-        const bucketIndex = this.#hash(key);
-        const list = this.#bucket[bucketIndex];
-
-        if (list.contains(key)) {
-            list.at(list.findIndex(key))[key] = value;
-        }
-
-        list.append({ [key]: value });
-        this.#entries += 1;
-    }
+    list.append({ [key]: value });
+    this.#entries++;
+    console.log(this.#entries);
+  }
 }
 
 const hashMap = new HashMap();
-console.log(hashMap.set("ben", "first value"));
-console.log(hashMap.set("neb", "second value"));
-console.log(hashMap.set("uuuuuu", "third value"));
+for (let i = 0; i < 13; i++) {
+  hashMap.set(`${i}ab`, "no empty");
+}
