db58e93e37407394882dce2c3cc6e026571b2217
justiceCodes <171641141+JusticeCodes00@users.noreply.github.com>
2026-01-29 23:24:14 +0100

    Add methods
    - Add remove()
    - Add length()

--- classes/HashMap.js
+++ classes/HashMap.js
@@ -1,118 +1,143 @@
 import LinkedList from "./LinkedList.js";
 
 export default class HashMap {
-  static HOW_MUCH_TO_GROW = 2;
+    static HOW_MUCH_TO_GROW = 2;
 
-  #bucket;
-  #capacity;
-  #loadFactor;
-  #entries = 0;
-  #currentKey = null;
+    #bucket;
+    #capacity;
+    #loadFactor;
+    #entries = 0;
+    #currentKey = null;
 
-  constructor() {
-    this.#capacity = 16;
-    this.#bucket = new Array(this.#capacity)
-      .fill(null)
-      .map(() => new LinkedList());
-    this.#loadFactor = 0.75;
-  }
-
-  #hash(key) {
-    if (typeof key !== "string") {
-      throw new Error("key must be of type String");
+    constructor() {
+        this.#capacity = 16;
+        this.#bucket = new Array(this.#capacity)
+            .fill(null)
+            .map(() => new LinkedList());
+        this.#loadFactor = 0.75;
     }
 
-    let hashCode = 0;
-    const primeNum = 31;
+    #hash(key) {
+        if (typeof key !== "string") {
+            throw new Error("key must be of type String");
+        }
 
-    for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNum * hashCode + key.charCodeAt(i)) % this.#capacity;
-    }
-    return hashCode;
-  }
+        let hashCode = 0;
+        const primeNum = 31;
 
-  #growBucket() {
-    this.#capacity = this.#capacity * HashMap.HOW_MUCH_TO_GROW;
-
-    const prevBucket = this.#bucket;
-
-    this.#bucket = new Array(this.#capacity)
-      .fill(null)
-      .map(() => new LinkedList());
-
-    for (let i = 0; i < prevBucket.length; i++) {
-      const current = prevBucket[i];
-      if (!current.size()) {
-        continue;
-      }
-
-      while (current.head()) {
-        const [[key, value]] = Object.entries(current.pop());
-        this.set(key, value);
-      }
-    }
-    for (let j = 0; j < this.#bucket.length; j++) {
-      console.log(this.#bucket[j].toString());
-    }
-    console.log("New Capacity: " + this.#capacity);
-  }
-
-  #match = (container) => this.#currentKey in container;
-
-  set(key, value) {
-    this.#currentKey = key;
-    const timeToGrow = this.#entries >= this.#loadFactor * this.#capacity;
-
-    if (timeToGrow) {
-      console.log(`Max entries exceeded(${this.#entries})\nGrowing bucket...`);
-      this.#growBucket();
+        for (let i = 0; i < key.length; i++) {
+            hashCode =
+                (primeNum * hashCode + key.charCodeAt(i)) % this.#capacity;
+        }
+        return hashCode;
     }
 
-    const bucketIndex = this.#hash(key);
-    const list = this.#bucket[bucketIndex];
+    #growBucket() {
+        this.#capacity = this.#capacity * HashMap.HOW_MUCH_TO_GROW;
 
-    if (list.contains(this.#match)) {
-      list.at(list.findIndex(this.#match))[key] = value;
+        const prevBucket = this.#bucket;
+
+        this.#bucket = new Array(this.#capacity)
+            .fill(null)
+            .map(() => new LinkedList());
+
+        for (let i = 0; i < prevBucket.length; i++) {
+            const current = prevBucket[i];
+            if (!current.size()) {
+                continue;
+            }
+
+            while (current.head()) {
+                const [[key, value]] = Object.entries(current.pop());
+                this.set(key, value);
+            }
+        }
+        for (let j = 0; j < this.#bucket.length; j++) {
+            console.log(this.#bucket[j].toString());
+        }
+        console.log("New Capacity: " + this.#capacity);
     }
 
-    list.append({ [key]: value });
+    #match = container => this.#currentKey in container;
 
-    this.#entries++;
-  }
+    set(key, value) {
+        this.#currentKey = key;
+        const timeToGrow = this.#entries >= this.#loadFactor * this.#capacity;
 
-  get(key) {
-    this.#currentKey = key;
-    const index = this.#hash(key);
-    let value = null;
+        if (timeToGrow) {
+            console.log(
+                `Max entries exceeded(${this.#entries})\nGrowing bucket...`
+            );
+            this.#growBucket();
+        }
 
-    const list = this.#bucket[index];
+        const bucketIndex = this.#hash(key);
+        const list = this.#bucket[bucketIndex];
 
-    const exist = list.contains(this.#match);
+        if (list.contains(this.#match)) {
+            list.at(list.findIndex(this.#match))[key] = value;
+        }
 
-    if (exist) {
-      value = list.at(list.findIndex(this.#match))[this.#currentKey];
+        list.append({ [key]: value });
+
+        this.#entries++;
     }
 
-    return value;
-  }
+    get(key) {
+        this.#currentKey = key;
+        const index = this.#hash(key);
+        let value = null;
 
-  has(key) {
-    this.#currentKey = key;
+        const list = this.#bucket[index];
 
-    const index = this.#hash(key);
+        const exist = list.contains(this.#match);
 
-    const list = this.#bucket[index];
+        if (exist) {
+            value = list.at(list.findIndex(this.#match))[this.#currentKey];
+        }
 
-    return list.contains(this.#match);
-  }
+        return value;
+    }
+
+    has(key) {
+        this.#currentKey = key;
+
+        const index = this.#hash(key);
+
+        const list = this.#bucket[index];
+
+        return list.contains(this.#match);
+    }
+
+    remove(key) {
+        this.#currentKey = key;
+        const index = this.#hash(key);
+        const list = this.#bucket[index];
+        let result = false;
+
+        if (list.contains(this.#match)) {
+            list.removeAt(list.findIndex(this.#match));
+            result = true;
+            this.#entries--;
+        }
+
+        return result;
+    }
+
+    length() {
+        return this.#entries;
+    }
 }
 
 // Test bucket growth
 
 const map = new HashMap();
 
-for (let i = 0; i < 32; i++) {
-  map.set(`ab${i}`, `${i}${i}${i}${i}`);
-}
+console.log(map.set("ben", "Hello"));
+// for (let i = 0; i < 32; i++) {
+//     map.set(`ab${i}`, `${i}${i}${i}${i}`);
+// }
 
-console.log(map.get("ab31"));
+console.log(map.remove("ben"));
+console.log(map.get("ben"));
+console.log(map.length());
